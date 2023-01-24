4c5fc36a915e0b1d9787601bbdbb1664ec972835
MMS <miro@quantum-leaps.com>
2023-01-24 13:12:19 -0500

    7.2.1

--- nucleo-l053r8/qutest/qutest_cpp.cpp
+++ nucleo-l053r8/qutest/qutest_cpp.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 //! Product: QUTEST port for the STM32 NUCLEO-L053R8 board
-// Last updated for version 7.2.0
-// Last updated on  2022-12-15
+// Last updated for version 7.2.1
+// Last updated on  2023-01-24
 //
 //                    Q u a n t u m  L e a P s
 //                    ------------------------
@@ -53,10 +53,10 @@ extern "C" void USART2_IRQHandler(void);
 
 // Local-scope objects -------------------------------------------------------
 // LED pins available on the board (just one user LED LD2--Green on PA.5)
-#define LED_LD2  (1U << 5)
+#define LD2_PIN  5U
 
 // Button pins available on the board (just one user Button B1 on PC.13)
-#define BTN_B1   (1U << 13)
+#define B1_PIN   13U
 
 #define __DIV(__PCLK, __BAUD)       (((__PCLK / 4) *25)/(__BAUD))
 #define __DIVMANT(__PCLK, __BAUD)   (__DIV(__PCLK, __BAUD)/100)
@@ -65,6 +65,10 @@ extern "C" void USART2_IRQHandler(void);
         * 16 + 50) / 100)
 #define __USART_BRR(__PCLK, __BAUD) \
     ((__DIVMANT(__PCLK, __BAUD) << 4)|(__DIVFRAQ(__PCLK, __BAUD) & 0x0F))
+
+// USART2 pins PA.2 and PA.3
+#define USART2_TX_PIN 2U
+#define USART2_RX_PIN 3U
 
 //............................................................................
 extern "C" {
@@ -98,26 +102,46 @@ bool QS::onStartup(void const *arg) {
     //
     SystemCoreClockUpdate();
 
+    // enable GPIOA clock port for the LED LD2
+    RCC->IOPENR |= (1U << 0U);
+
+    // configure LED (PA.5) pin as push-pull output, no pull-up, pull-down
+    GPIOA->MODER   &= ~((3U << 2U*LD2_PIN));
+    GPIOA->MODER   |=  ((1U << 2U*LD2_PIN));
+    GPIOA->OTYPER  &= ~((1U <<    LD2_PIN));
+    GPIOA->OSPEEDR &= ~((3U << 2U*LD2_PIN));
+    GPIOA->OSPEEDR |=  ((1U << 2U*LD2_PIN));
+    GPIOA->PUPDR   &= ~((3U << 2U*LD2_PIN));
+
+    // enable GPIOC clock port for the Button B1
+    RCC->IOPENR |=  (1U << 2U);
+
+    // configure Button (PC.13) pins as input, no pull-up, pull-down
+    GPIOC->MODER   &= ~(3U << 2U*B1_PIN);
+    GPIOC->OSPEEDR &= ~(3U << 2U*B1_PIN);
+    GPIOC->OSPEEDR |=  (1U << 2U*B1_PIN);
+    GPIOC->PUPDR   &= ~(3U << 2U*B1_PIN);
+
     // enable peripheral clock for USART2
-    RCC->IOPENR  |= ( 1U <<  0);   // Enable GPIOA clock
-    RCC->APB1ENR |= ( 1U << 17);   // Enable USART#2 clock
+    RCC->IOPENR  |= ( 1U <<  0U);  // Enable GPIOA clock
+    RCC->APB1ENR |= ( 1U << 17U);  // Enable USART#2 clock
 
     // Configure PA3 to USART2_RX, PA2 to USART2_TX
-    GPIOA->AFR[0] &= ~((15U << 4* 3) | (15U << 4* 2) );
-    GPIOA->AFR[0] |=  (( 4U << 4* 3) | ( 4U << 4* 2) );
-    GPIOA->MODER  &= ~(( 3U << 2* 3) | ( 3U << 2* 2) );
-    GPIOA->MODER  |=  (( 2U << 2* 3) | ( 2U << 2* 2) );
+    GPIOA->AFR[0] &= ~((15U << 4U*USART2_RX_PIN) | (15U << 4U*USART2_TX_PIN) );
+    GPIOA->AFR[0] |=  (( 4U << 4U*USART2_RX_PIN) | ( 4U << 4U*USART2_TX_PIN) );
+    GPIOA->MODER  &= ~(( 3U << 2U*USART2_RX_PIN) | ( 3U << 2U*USART2_TX_PIN) );
+    GPIOA->MODER  |=  (( 2U << 2U*USART2_RX_PIN) | ( 2U << 2U*USART2_TX_PIN) );
 
     USART2->BRR  = __USART_BRR(SystemCoreClock, 115200U);  // baud rate
-    USART2->CR3  = 0x0000 |       // no flow control
-                   (1U << 12);    // disable overrun detection (OVRDIS)
-    USART2->CR2  = 0x0000;        // 1 stop bit
-    USART2->CR1  = ((1U <<  2) |  // enable RX
-                    (1U <<  3) |  // enable TX
-                    (1U <<  5) |  // enable RX interrupt
-                    (0U << 12) |  // 8 data bits
-                    (0U << 28) |  // 8 data bits
-                    (1U <<  0) ); // enable USART
+    USART2->CR3  = 0x0000U |      // no flow control
+                   (1U << 12U);   // disable overrun detection (OVRDIS)
+    USART2->CR2  = 0x0000U;       // 1 stop bit
+    USART2->CR1  = ((1U <<  2U) | // enable RX
+                    (1U <<  3U) | // enable TX
+                    (1U <<  5U) | // enable RX interrupt
+                    (0U << 12U) | // 8 data bits
+                    (0U << 28U) | // 8 data bits
+                    (1U <<  0U) );// enable USART
 
     // explicitly set NVIC priorities of all Cortex-M interrupts used
     NVIC_SetPriorityGrouping(0U);
@@ -153,8 +177,6 @@ void QS::onFlush(void) {
             break;
         }
     }
-    while ((USART2->ISR & (1U << 7U)) == 0U) { // while TXE not empty
-    }
 }
 //............................................................................
 //! callback function to reset the target (to be implemented in the BSP)
@@ -163,7 +185,7 @@ void QS::onReset(void) {
 }
 //............................................................................
 void QS::doOutput(void) {
-    if ((USART2->ISR & (1U << 7)) != 0) {  // is TXE empty?
+    if ((USART2->ISR & (1U << 7U)) != 0U) {  // is TXE empty?
         QF_INT_DISABLE();
         std::uint16_t b = getByte();
         QF_INT_ENABLE();
@@ -178,9 +200,13 @@ void QS::onTestLoop() {
     rxPriv_.inTestLoop = true;
     while (rxPriv_.inTestLoop) {
 
+        // toggle an LED LD2 on and then off (not enough LEDs, see NOTE02)
+        GPIOA->BSRR = (1U << LD2_PIN);         // turn LED[n] on
+        GPIOA->BSRR = (1U << (LD2_PIN + 16U)); // turn LED[n] off
+
         rxParse();  // parse all the received bytes
 
-        if ((USART2->ISR & (1U << 7)) != 0) {  // is TXE empty?
+        if ((USART2->ISR & (1U << 7U)) != 0U) {  // is TXE empty?
             std::uint16_t b = getByte();
             if (b != QS_EOD) {  // not End-Of-Data?
                 USART2->TDR = (b & 0xFFU);  // put into the DR register
--- nucleo-l053r8/qutest/qutest_port.c
+++ nucleo-l053r8/qutest/qutest_port.c
@@ -1,13 +1,13 @@
-/*****************************************************************************
+/*============================================================================
 * Product: QUTEST port for NUCLEO-L053R8 board
-* Last updated for version 7.2.0
-* Last updated on  2022-12-15
+* Last updated for version 7.2.1
+* Last updated on  2023-01-24
 *
 *                    Q u a n t u m  L e a P s
 *                    ------------------------
 *                    Modern Embedded Software
 *
-* Copyright (C) 2005 Quantum Leaps, LLC. All rights reserved.
+* Copyright (C) 2005 Quantum Leaps. All rights reserved.
 *
 * This program is open source software: you can redistribute it and/or
 * modify it under the terms of the GNU General Public License as published
@@ -30,7 +30,7 @@
 * Contact information:
 * <www.state-machine.com/licensing>
 * <info@state-machine.com>
-*****************************************************************************/
+============================================================================*/
 #ifndef Q_SPY
     #error "Q_SPY must be defined to compile qutest_port.c"
 #endif /* Q_SPY */
@@ -51,10 +51,10 @@ void USART2_IRQHandler(void);
 
 /* Local-scope defines -----------------------------------------------------*/
 /* LED pins available on the board (just one user LED LD2--Green on PA.5) */
-#define LED_LD2  (1U << 5)
+#define LD2_PIN  5U
 
 /* Button pins available on the board (just one user Button B1 on PC.13) */
-#define BTN_B1   (1U << 13)
+#define B1_PIN   13U
 
 #define __DIV(__PCLK, __BAUD)       (((__PCLK / 4) *25)/(__BAUD))
 #define __DIVMANT(__PCLK, __BAUD)   (__DIV(__PCLK, __BAUD)/100)
@@ -63,6 +63,10 @@ void USART2_IRQHandler(void);
         * 16 + 50) / 100)
 #define __USART_BRR(__PCLK, __BAUD) \
     ((__DIVMANT(__PCLK, __BAUD) << 4)|(__DIVFRAQ(__PCLK, __BAUD) & 0x0F))
+
+/* USART2 pins PA.2 and PA.3 */
+#define USART2_TX_PIN 2U
+#define USART2_RX_PIN 3U
 
 /*..........................................................................*/
 /*
@@ -73,7 +77,7 @@ void USART2_IRQHandler(void);
 */
 void USART2_IRQHandler(void) { /* used in QS-RX (see NOTE0) */
     /* is RX register NOT empty? */
-    while ((USART2->ISR & (1U << 5)) != 0) {
+    while ((USART2->ISR & (1U << 5U)) != 0U) {
         uint32_t b = USART2->RDR;
         QS_RX_PUT(b);
     }
@@ -94,26 +98,46 @@ uint8_t QS_onStartup(void const *arg) {
     */
     SystemCoreClockUpdate();
 
+    /* enable GPIOA clock port for the LED LD2 */
+    RCC->IOPENR |= (1U << 0U);
+
+    /* configure LED (PA.5) pin as push-pull output, no pull-up, pull-down */
+    GPIOA->MODER   &= ~((3U << 2U*LD2_PIN));
+    GPIOA->MODER   |=  ((1U << 2U*LD2_PIN));
+    GPIOA->OTYPER  &= ~((1U <<    LD2_PIN));
+    GPIOA->OSPEEDR &= ~((3U << 2U*LD2_PIN));
+    GPIOA->OSPEEDR |=  ((1U << 2U*LD2_PIN));
+    GPIOA->PUPDR   &= ~((3U << 2U*LD2_PIN));
+
+    /* enable GPIOC clock port for the Button B1 */
+    RCC->IOPENR |=  (1U << 2U);
+
+    /* configure Button (PC.13) pins as input, no pull-up, pull-down */
+    GPIOC->MODER   &= ~(3U << 2U*B1_PIN);
+    GPIOC->OSPEEDR &= ~(3U << 2U*B1_PIN);
+    GPIOC->OSPEEDR |=  (1U << 2U*B1_PIN);
+    GPIOC->PUPDR   &= ~(3U << 2U*B1_PIN);
+
     /* enable peripheral clock for USART2 */
-    RCC->IOPENR  |= ( 1U <<  0);   /* Enable GPIOA clock   */
-    RCC->APB1ENR |= ( 1U << 17);   /* Enable USART#2 clock */
+    RCC->IOPENR  |= ( 1U <<  0U);  /* Enable GPIOA clock   */
+    RCC->APB1ENR |= ( 1U << 17U);  /* Enable USART#2 clock */
 
     /* Configure PA3 to USART2_RX, PA2 to USART2_TX */
-    GPIOA->AFR[0] &= ~((15U << 4* 3) | (15U << 4* 2) );
-    GPIOA->AFR[0] |=  (( 4U << 4* 3) | ( 4U << 4* 2) );
-    GPIOA->MODER  &= ~(( 3U << 2* 3) | ( 3U << 2* 2) );
-    GPIOA->MODER  |=  (( 2U << 2* 3) | ( 2U << 2* 2) );
+    GPIOA->AFR[0] &= ~((15U << 4U*USART2_RX_PIN) | (15U << 4U*USART2_TX_PIN) );
+    GPIOA->AFR[0] |=  (( 4U << 4U*USART2_RX_PIN) | ( 4U << 4U*USART2_TX_PIN) );
+    GPIOA->MODER  &= ~(( 3U << 2U*USART2_RX_PIN) | ( 3U << 2U*USART2_TX_PIN) );
+    GPIOA->MODER  |=  (( 2U << 2U*USART2_RX_PIN) | ( 2U << 2U*USART2_TX_PIN) );
 
     USART2->BRR  = __USART_BRR(SystemCoreClock, 115200U);  /* baud rate */
-    USART2->CR3  = 0x0000 |       /* no flow control */
-                   (1U << 12);    /* disable overrun detection (OVRDIS) */
-    USART2->CR2  = 0x0000;        /* 1 stop bit      */
-    USART2->CR1  = ((1U <<  2) |  /* enable RX       */
-                    (1U <<  3) |  /* enable TX       */
-                    (1U <<  5) |  /* enable RX interrupt */
-                    (0U << 12) |  /* 8 data bits     */
-                    (0U << 28) |  /* 8 data bits     */
-                    (1U <<  0) ); /* enable USART    */
+    USART2->CR3  = 0x0000U |      /* no flow control */
+                   (1U << 12U);   /* disable overrun detection (OVRDIS) */
+    USART2->CR2  = 0x0000U;       /* 1 stop bit      */
+    USART2->CR1  = ((1U <<  2U) | /* enable RX       */
+                    (1U <<  3U) | /* enable TX       */
+                    (1U <<  5U) | /* enable RX interrupt */
+                    (0U << 12U) | /* 8 data bits     */
+                    (0U << 28U) | /* 8 data bits     */
+                    (1U <<  0U) );/* enable USART    */
 
     /* explicitly set NVIC priorities of all Cortex-M interrupts used */
     NVIC_SetPriorityGrouping(0U);
@@ -172,13 +196,13 @@ void QS_onTestLoop() {
     QS_rxPriv_.inTestLoop = true;
     while (QS_rxPriv_.inTestLoop) {
 
-        /* toggle an LED on and then off (not enough LEDs, see NOTE02) */
-        GPIOA->BSRR |= (LED_LD2);        /* turn LED[n] on  */
-        GPIOA->BSRR |= (LED_LD2 << 16);  /* turn LED[n] off */
+        /* toggle an LED LD2 on and then off (not enough LEDs, see NOTE02) */
+        GPIOA->BSRR = (1U << LD2_PIN);         /* turn LED[n] on  */
+        GPIOA->BSRR = (1U << (LD2_PIN + 16U)); /* turn LED[n] off */
 
         QS_rxParse();  /* parse all the received bytes */
 
-        if ((USART2->ISR & (1U << 7)) != 0) {  /* is TXE empty? */
+        if ((USART2->ISR & (1U << 7U)) != 0U) {  /* is TXE empty? */
             QF_INT_DISABLE();
             uint16_t b = QS_getByte();
             QF_INT_ENABLE();
